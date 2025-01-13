11f23f09760668022db818fda8ff6797957747a6
F33$H <TheDron2006@gmail.com>
2025-01-13 19:18:30 +0300

    camera FOV depends on screen resolution

---
+++ Assets/Scripts/Level/CameraBehaviour/CameraFOVDependsOnScreenResolution.cs
@@ -0,0 +1,11 @@
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class CameraFOVDependsOnScreenResolution : MonoBehaviour
+{
+    public float FullHDFieldOfView = 30f;
+    void Start()
+    {
+        GetComponent<Camera>().fieldOfView = Screen.width / 1920f * FullHDFieldOfView;
+    }
+}
