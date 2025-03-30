f60f96a6ca284633e91c1074cde8a731234ad8f0
F33$H <TheDron2006@gmail.com>
2025-03-31 01:15:03 +0300

    added unfinished multisprite editor tool

--- Assets/EditorScripts/UpdateMultiSprites.cs
+++ Assets/EditorScripts/UpdateMultiSprites.cs
@@ -4,10 +4,14 @@ using System;
 using UnityEngine.UIElements;
 using System.IO;
 using Unity.VisualScripting;
+using System.Linq;
 
 public class UpdateMultiSprites : Editor
 {
     const string CHARACTER_SPRITES_DIR = "\\Assets\\Sprites\\Character";
+
+    public CharacterPartVisualManager TargetVisual;
+    public static CharacterPartVisualManager StaticTargetVisual;
 
     [MenuItem("Tools/Update character textures")]
     private static void UpdateCharacterTextures()
@@ -15,8 +19,52 @@ public class UpdateMultiSprites : Editor
         string[] files = Directory.GetFiles(Directory.GetCurrentDirectory() + CHARACTER_SPRITES_DIR, "*png", SearchOption.AllDirectories);
         foreach (var file in files)
         {
-            Debug.Log(file);
-            Debug.Log(Resources.Load(file));
+            string currentAnimationShortTitle = null;
+            Sprite[] currentSprites = new Sprite[Enum.GetNames(typeof(CharacterPartVisualManager.CharPartsAnimation)).Length];
+            Sprite currentSampleSprite = null;
+            int currentAnimationIndex = 0;
+
+            UnityEngine.Object[] loadedItems = AssetDatabase.LoadAllAssetsAtPath(file.Substring(Directory.GetCurrentDirectory().Length + 1));
+            foreach (var item in loadedItems)
+            {
+                string newAnimationShortTitle = null;
+
+                if (item is Sprite spriteItem)
+                {
+                    currentSprites[currentAnimationIndex] = spriteItem;
+                    if (currentAnimationIndex == (int)CharacterPartVisualManager.SAMPLE_ANIMATION)
+                    {
+                        currentSampleSprite = spriteItem;
+                    }
+
+                    newAnimationShortTitle = spriteItem.name.Substring(0, spriteItem.name.IndexOf('.')) + (spriteItem.name.IndexOf('_') != -1 ? spriteItem.name.Substring(spriteItem.name.IndexOf('_')) : "");
+                    currentAnimationIndex++;
+
+                    if (newAnimationShortTitle != currentAnimationShortTitle)
+                    {
+                        StaticTargetVisual.SampleSprites.Add(currentSampleSprite, currentSprites);
+                        Debug.Log(currentSampleSprite + " : ");
+                        foreach (Sprite sprite in currentSprites)
+                        {
+                            Debug.Log(sprite);
+                        }
+
+                        currentSprites = new Sprite[Enum.GetNames(typeof(CharacterPartVisualManager.CharPartsAnimation)).Length];
+                        currentSampleSprite = null;
+                        currentAnimationIndex = 0;
+                    }
+
+                    currentAnimationShortTitle = newAnimationShortTitle;
+                }
+            }
         }
+        
+
+    }
+
+    void OnGUI()
+    {
+        TargetVisual = EditorGUILayout.ObjectField(TargetVisual, typeof(CharacterPartVisualManager), true) as CharacterPartVisualManager;
+        StaticTargetVisual = TargetVisual;
     }
 }
--- Assets/Scripts/Level/Character/CharacterInfo/CharacterPart/CharacterPartVisualManager.cs
+++ Assets/Scripts/Level/Character/CharacterInfo/CharacterPart/CharacterPartVisualManager.cs
@@ -1,11 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.WSA;
 
 public class CharacterPartVisualManager : MonoBehaviour
 {
+    public const AnimatedCharacterParts SAMPLE_ANIMATION = AnimatedCharacterParts.Body;
+
+    const string CHARACTER_SPRITES_DIR = "\\Assets\\Sprites\\Character";
+
+    /// <summary>
+    /// Note: if you will add new item, they MUST be sorted by name alphabetically and be same as sprite name
+    /// </summary>
     public enum CharPartsAnimation : int
     {
         Aim,
@@ -28,62 +36,22 @@ public class CharacterPartVisualManager : MonoBehaviour
         WakeUp
     }
 
-    public enum AnimatedCharacterParts
+    /// <summary>
+    /// Note: if you will add new item, they MUST be sorted by name alphabetically and be same as sprite name
+    /// </summary>
+    public enum AnimatedCharacterParts : int
     {
-        Body,
         BodyArmor,
         BodyCape,
         BodyHeavyArmor,
+        Body,
         EyesDefault,
         EyesGlasses,
-        Head,
         HeadHeavyHelmet,
         HeadHelmet,
+        Head,
         LegsArmor
     }
 
-    [Serializable] private class CharacterPartClipsContent
-    {
-        public string Title;
-        public AnimationClip[] AnimationClips;
-    }
-
-    public static CharacterPartVisualManager Instance;
-
-    private void Awake()
-    {
-        if (Instance != null) throw new UnityException("limit of 1 CharacterPartVisualManager instance per scene");
-        Instance = this;
-
-        _characterPartAnimationClipsDict = new();
-        foreach (var characterAnimationClip in _characterPartAnimationClips)
-        {
-            _characterPartAnimationClipsDict.Add(characterAnimationClip.AnimationClips[0], characterAnimationClip.AnimationClips);
-        }
-    }
-
-    [SerializeField] private CharacterPartClipsContent[] _characterPartAnimationClips;
-    private Dictionary<AnimationClip, AnimationClip[]> _characterPartAnimationClipsDict;
-
-    public AnimationClip GetCharacterPartClip(AnimationClip sampleClip, AnimatedCharacterParts animatedPart)
-    {
-        return _characterPartAnimationClipsDict[sampleClip][(int)animatedPart];
-    }
-
-    private static List<Sprite> GetSpritesFromClip(AnimationClip clip)
-    {
-        var sprites = new List<Sprite>();
-        if (clip != null)
-        {
-            foreach (var binding in AnimationUtility.GetObjectReferenceCurveBindings(clip))
-            {
-                var keyframes = AnimationUtility.GetObjectReferenceCurve(clip, binding);
-                foreach (var frame in keyframes)
-                {
-                    sprites.Add((Sprite)frame.value);
-                }
-            }
-        }
-        return sprites;
-    }
+    public Dictionary<Sprite, Sprite[]> SampleSprites = new();
 }
