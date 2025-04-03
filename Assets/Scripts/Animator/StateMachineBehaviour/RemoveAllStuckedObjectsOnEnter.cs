601fb3e18247906ebaef0e19b0bca5e86991660d
F33$H <TheDron2006@gmail.com>
2025-04-03 13:29:15 +0300

    fixed RemoveStuckObjects class not working

--- Assets/Scripts/Animator/StateMachineBehaviour/RemoveAllStuckedObjectsOnEnter.cs
+++ Assets/Scripts/Animator/StateMachineBehaviour/RemoveAllStuckedObjectsOnEnter.cs
@@ -4,7 +4,7 @@ public class RemoveAllStuckedObjectsOnEnter : StateMachineBehaviour
 {
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (animator.transform.parent.TryGetComponent(out AbstractCharacterComponent charComponent)) 
+        if (animator.TryGetComponent(out AbstractCharacterComponent charComponent)) 
         {
             charComponent.CharComponents.CharacterStuckedObjects.RemoveAllStuckedObjects();
         }
