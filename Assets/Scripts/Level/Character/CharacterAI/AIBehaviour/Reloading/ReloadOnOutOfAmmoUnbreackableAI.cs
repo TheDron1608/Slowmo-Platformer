833805db90a40d8d14f6637f51031cabd786bf99
F33$H <TheDron2006@gmail.com>
2025-04-22 22:18:02 +0300

    added ReloadOnOutOfAmmoUnbreackableAI

---
+++ Assets/Scripts/Level/Character/CharacterAI/AIBehaviour/Reloading/ReloadOnOutOfAmmoUnbreackableAI.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ReloadOnOutOfAmmoUnbreackableAI : AbstractAIReloading
+{
+    private void FixedUpdate()
+    {
+        if (
+            !CharComponents.CharacterReloading.GetIsReloading() &&
+            CharComponents.CharacterHolding.CurrentHoldObject != null &&
+            CharComponents.CharacterHolding.CurrentHoldObject.TryGetComponent(out RangedWeapon rangedWeapon)
+            )
+        {
+            if (rangedWeapon.GetIsNeedReload())
+            {
+                CharComponents.CharacterReloading.TryReload();
+            }
+        }
+    }
+}
