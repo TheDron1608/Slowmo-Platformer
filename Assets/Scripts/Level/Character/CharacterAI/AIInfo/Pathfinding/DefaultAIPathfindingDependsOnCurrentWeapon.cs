7e5cefffc8f96aaacbc81ef440967a953472b877
F33$H <TheDron2006@gmail.com>
2025-05-12 14:11:17 +0300

    added DefaultAIPathfindingDependsOnCurrentWeapon AIBehaviour class

---
+++ Assets/Scripts/Level/Character/CharacterAI/AIInfo/Pathfinding/DefaultAIPathfindingDependsOnCurrentWeapon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class DefaultAIPathfindingDependsOnCurrentWeapon : DefaultAIPathfinding
+{
+    public float MinDistanceToTargetWithRangedWeapon = 3f;
+
+    protected override void OnUpdateInfo()
+    {
+        if (
+            (CharComponents.CharacterHolding.CurrentHoldObject?.GetComponent<MeleeWeapon>() != null && CharComponents.CharacterClumsyness.ClumsyMeleeAttack) ||
+            (CharComponents.CharacterHolding.CurrentHoldObject?.GetComponent<RangedWeapon>() != null && CharComponents.CharacterClumsyness.ClumsyRangedAttack)
+            )
+        {
+            CanJumpToTarget = false;
+        }
+        else
+        {
+            CanJumpToTarget = true;
+        }
+
+        if (PathTarget.HasValue && Vector2.Distance(PathTarget.Value, CharComponents.transform.position) < MinDistanceToTargetWithRangedWeapon)
+        {
+            PathTarget = CharComponents.transform.position;
+        }
+
+        base.OnUpdateInfo();
+    }
+}
