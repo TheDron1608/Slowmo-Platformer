88265b871f1eca0d4301a329ba83df32a53300dd
F33$H <TheDron2006@gmail.com>
2025-05-29 14:16:25 +0300

    fixed objects become invisible when chaing z layer

--- Assets/Scripts/Level/CameraBehaviour/CameraTrack.cs
+++ Assets/Scripts/Level/CameraBehaviour/CameraTrack.cs
@@ -7,12 +7,9 @@ public class CameraTrack : MonoBehaviour
     public GameObject TrackObject;
     public float TrackSpeed = 5f;
     public float TrackMouseVelocity = 0.1625f;
-    public float LayerAppearDistance = 1.5f;
-    public float OvergoundAppearOffset = 2f;
 
     private Rigidbody _rigidBodyComponent;
     private MultiZLayerCamera _multiZLayerCameraComponent;
-    private Camera _cameraComponent;
 
     public void InstantMoveToTrackObject()
     {
@@ -25,10 +22,9 @@ public class CameraTrack : MonoBehaviour
             );
     }
 
-    private void LateUpdate()
+    private void Update()
     {
         UpdateCameraVecloity();
-        UpdateLayerAlpha();
     }
 
     private void UpdateCameraVecloity()
@@ -45,24 +41,10 @@ public class CameraTrack : MonoBehaviour
         _rigidBodyComponent.linearVelocity = (trackTargetPosition - transform.position) * TrackSpeed;
     }
 
-    private void UpdateLayerAlpha()
-    {
-        for (int i = 0; i < LayerManager.Instance.ZLayers.Count; i++)
-        {
-            float distanceToLayer = LayerManager.Instance.ZLayers[i].transform.position.z - transform.position.z - _cameraComponent.nearClipPlane;
-
-            LayerManager.Instance.ZLayers[i].LayerAlpha = new(
-                NumberMath.LimitFloatBetweenZeroAndOne(distanceToLayer / LayerAppearDistance),
-                NumberMath.LimitFloatBetweenZeroAndOne(distanceToLayer - OvergoundAppearOffset / LayerAppearDistance)
-                );
-        }
-    }
-
     private void Awake()
     {
         if (!TryGetComponent(out _rigidBodyComponent)) throw new UnityException("RigidBodyComponent not found");
         if (!TryGetComponent(out _multiZLayerCameraComponent)) throw new UnityException("MultiZLayerCamera component not found");
-        if (!TryGetComponent(out _cameraComponent)) throw new UnityException("Camera component not found");
 
         InstantMoveToTrackObject();
     }
--- Assets/Scripts/Level/CameraBehaviour/MultiZLayerCamera.cs
+++ Assets/Scripts/Level/CameraBehaviour/MultiZLayerCamera.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 public class MultiZLayerCamera : MonoBehaviour
 {
     public float ZoomOutDistance = 15f;
+    public float LayerAppearDistance = 1.5f;
+    public float OvergoundAppearOffset = 2f;
 
     [SerializeField]
     private ZIndexLayer _startZLayer;
+
+    private Camera _cameraComponent;
 
     private int _currentIndex = 1;
     public int CurrentZIndex
@@ -30,6 +34,25 @@ public class MultiZLayerCamera : MonoBehaviour
     private void Awake()
     {
         _currentIndex = _startZLayer.ZIndex;
+        if (!TryGetComponent(out _cameraComponent)) throw new UnityException("Camera component not found");
+    }
+
+    private void LateUpdate()
+    {
+        UpdateLayerAlpha();
+    }
+
+    private void UpdateLayerAlpha()
+    {
+        for (int i = 0; i < LayerManager.Instance.ZLayers.Count; i++)
+        {
+            float distanceToLayer = LayerManager.Instance.ZLayers[i].transform.position.z - transform.position.z - _cameraComponent.nearClipPlane;
+
+            LayerManager.Instance.ZLayers[i].LayerAlpha = new(
+                NumberMath.LimitFloatBetweenZeroAndOne(distanceToLayer / LayerAppearDistance),
+                NumberMath.LimitFloatBetweenZeroAndOne(distanceToLayer - OvergoundAppearOffset / LayerAppearDistance)
+                );
+        }
     }
 
     public ZIndexLayer GetCurrentZIndexLayer()
