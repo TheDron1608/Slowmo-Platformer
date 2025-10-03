b081729db42f4b175e96621ab47c9eb981bfdaa1
F33$H <TheDron2006@gmail.com>
2025-10-03 09:58:41 +0300

    fixed door damagable if already broken

--- Assets/Scripts/Level/InteractableObjects/Furniture/BreakableDoor.cs
+++ Assets/Scripts/Level/InteractableObjects/Furniture/BreakableDoor.cs
@@ -36,15 +36,21 @@ public class BreakableDoor : BreakableObject
             _brokenPartsParticleSpawners[i].SpawnParticle();
         }
 
-        GetComponent<SpriteRenderer>().flipX = transform.position.x < breaker.transform.position.x;
-        GetComponent<OnInteractToggleOpenDoor>().IsOpen = true;
-        GetComponent<OnInteractToggleOpenDoor>().enabled = false;
-        GetComponent<Animator>()?.SetBool(ANIMATOR_DESTROYED_PROP_NAME, true);
 
         for (int i = 0; i < _brokenPartsParticleSpawners.Count; i++)
         {
             _brokenPartsParticleSpawners[i].transform.rotation = breaker.transform.rotation;
             _brokenPartsParticleSpawners[i].SpawnParticle();
+        }
+
+        GetComponent<SpriteRenderer>().flipX = transform.position.x < breaker.transform.position.x;
+        GetComponent<OnInteractToggleOpenDoor>().IsOpen = true;
+        GetComponent<OnInteractToggleOpenDoor>().enabled = false;
+        GetComponent<Animator>()?.SetBool(ANIMATOR_DESTROYED_PROP_NAME, true);
+        if (TryGetComponent(out DamagableObject damagableObject))
+        {
+            damagableObject.HitableByMeleeProjectiles = false;
+            damagableObject.HitableByRangedProjectiles = false;
         }
     }
 }
