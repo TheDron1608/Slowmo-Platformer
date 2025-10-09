d580c0af09730230bccf2d279b6f45f1484ecef2
F33$H <TheDron2006@gmail.com>
2025-10-09 13:57:41 +0300

    added light  + tiny dynamic light optimization

--- Assets/Scripts/Level/DynamicParameters/DynamicLightSortingLayer.cs
+++ Assets/Scripts/Level/DynamicParameters/DynamicLightSortingLayer.cs
@@ -11,26 +11,30 @@ public class DynamicLightSortingLayer : MonoBehaviour
     private FieldInfo _sortingLayer;
     private MethodInfo _updateSortingLayer;
     private Light2D _lightComponent;
+    private int[] _currentValue;
 
     private void Awake()
     {
         _sortingLayer = GetComponent<Light2D>().GetType().GetField("m_ApplyToSortingLayers", BindingFlags.NonPublic | BindingFlags.Instance);
         _updateSortingLayer = GetComponent<Light2D>().GetType().GetMethod("MarkForUpdate", BindingFlags.NonPublic | BindingFlags.Instance);
         _lightComponent = GetComponent<Light2D>();
+        _currentValue = _sortingLayer.GetValue(_lightComponent) as int[];
     }
 
     public int[] SortingLayer
     {
         get
         {
-            return _sortingLayer.GetValue(_lightComponent) as int[];
+            return _currentValue;
         }
         set
         {
-            if (value == SortingLayer) return;
+            if (value == _currentValue) return;
 
             _sortingLayer.SetValue(_lightComponent, value);
             _updateSortingLayer.Invoke(_lightComponent, new object[0]);
+
+            _currentValue = value;
         }
     }
 }
--- Assets/Scripts/Level/DynamicParameters/DynamicShadowCasterSortingLayer.cs
+++ Assets/Scripts/Level/DynamicParameters/DynamicShadowCasterSortingLayer.cs
@@ -10,23 +10,28 @@ public class DynamicShadowCasterSortingLayer : MonoBehaviour
 {
     private FieldInfo _sortingLayer;
     private ShadowCaster2D _shadowCasterComponent;
+    private int[] _currentvalue;
 
     private void Awake()
     {
         _sortingLayer = GetComponent<ShadowCaster2D>().GetType().GetField("m_ApplyToSortingLayers", BindingFlags.NonPublic | BindingFlags.Instance);
         _shadowCasterComponent = GetComponent<ShadowCaster2D>();
+        _currentvalue = _sortingLayer.GetValue(_shadowCasterComponent) as int[];
     }
 
     public int[] SortingLayer
     {
         get
         {
-            return _sortingLayer.GetValue(_shadowCasterComponent) as int[];
+            return _currentvalue;
         }
         set
         {
-            if (_sortingLayer == null) Debug.Log(gameObject.name);
+            if (value == _currentvalue) return;
+
             _sortingLayer.SetValue(_shadowCasterComponent, value);
+
+            _currentvalue = value;
         }
     }
 }
