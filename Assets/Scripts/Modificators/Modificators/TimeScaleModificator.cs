12932821d8df1a6d3456233ecc19e41ce08ca3cd
F33$H <TheDron2006@gmail.com>
2026-01-29 13:24:39 +0300

    added TimeScale modificators

---
+++ Assets/Scripts/Modificators/Modificators/TimeScaleModificator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class TimeScaleModificator : AbstractMultiplierableModificator
+{
+    public float TimeScaleMultiplier = 1f;
+
+    public override void OnLevelPreGenerated()
+    {
+        base.OnLevelPreGenerated();
+
+        Time.timeScale *= TimeScaleMultiplier;
+        if (TimeScaleMultiplier < 0f)
+        {
+            Time.fixedDeltaTime *= TimeScaleMultiplier;
+        }
+    }
+
+    public override void OnLevelFinished()
+    {
+        base.OnLevelFinished();
+
+        Time.timeScale /= TimeScaleMultiplier;
+        if (TimeScaleMultiplier < 0f)
+        {
+            Time.fixedDeltaTime /= TimeScaleMultiplier;
+        }
+    }
+}
