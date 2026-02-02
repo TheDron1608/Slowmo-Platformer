80a807619dd12138baef947ccd90f824e6d70d53
F33$H <TheDron2006@gmail.com>
2026-02-02 03:47:49 +0300

    new enemies modificator 1

---
+++ Assets/Scripts/Modificators/Modificators/AddEnemyPoolItemModificator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class AddEnemyPoolItemModificator : AbstractModificator
+{
+    EnemySpawnInfo AddedEnemyItem;
+
+    public override void OnModificatorAdded()
+    {
+        base.OnModificatorAdded();
+
+        SpawnManager.Instance.EnemyPool.Add(AddedEnemyItem);
+    }
+
+    public override void OnModificatorRemoved()
+    {
+        base.OnModificatorRemoved();
+
+        SpawnManager.Instance.EnemyPool.Remove(AddedEnemyItem);
+    }
+}
