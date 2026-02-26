c5dec529b3a6152dab8e1824d44077fff947b007
F33$H <TheDron2006@gmail.com>
2026-02-26 11:10:07 +0300

    added EnemyHealthUp and EnemySpeedUp modificators

---
+++ Assets/Scripts/Modificators/Modificators/MultiplyTeamMemberHealthModificator.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Mathematics;
+
+public class MultiplyTeamMemberHealthModificator : AbstractCharactersModificator
+{
+    public float HealthMultiplier = 1f;
+
+    protected override void OnCharacterAffected(CharacterComponentsManager character)
+    {
+        character.CharacterHealth.ApplyMaxHealth(character.CharacterHealth.MaxHealth * HealthMultiplier * ModificatorMultiplier, null);
+        character.CharacterHealth.SetHealth(character.CharacterHealth.CurrentHealth * HealthMultiplier * ModificatorMultiplier, null, null);
+    }
+
+    protected override void OnCharacterRemovedAffect(CharacterComponentsManager character)
+    {
+        character.CharacterHealth.SetHealth(character.CharacterHealth.CurrentHealth / HealthMultiplier / ModificatorMultiplier, null, null);
+        character.CharacterHealth.ApplyMaxHealth(character.CharacterHealth.MaxHealth / HealthMultiplier / ModificatorMultiplier, null);
+    }
+}
---
+++ Assets/Scripts/Modificators/Modificators/MultiplyTeamMemberSpeedModificator.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Mathematics;
+
+public class MultiplyTeamMemberSpeedModificator : AbstractCharactersModificator
+{
+    public float SpeedMultiplier = 1f;
+    public float JumpMultiplier = 1f;
+
+    protected override void OnCharacterAffected(CharacterComponentsManager character)
+    {
+        character.CharacterMoving.Speed *= SpeedMultiplier * ModificatorMultiplier;
+        character.CharacterJumping.JumpForce *= JumpMultiplier * ModificatorMultiplier;
+    }
+
+    protected override void OnCharacterRemovedAffect(CharacterComponentsManager character)
+    {
+        character.CharacterMoving.Speed /= SpeedMultiplier * ModificatorMultiplier;
+        character.CharacterJumping.JumpForce /= JumpMultiplier * ModificatorMultiplier;
+    }
+}
