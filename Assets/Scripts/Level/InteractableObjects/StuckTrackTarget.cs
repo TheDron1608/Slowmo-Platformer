bb4bdaa51d8d3a8619adc63acabd1481da8150df
F33$H <TheDron2006@gmail.com>
2026-05-07 02:42:20 +0300

    fixed DestroyedReference on stucked to collider destroyed

--- Assets/Scripts/Level/InteractableObjects/StuckTrackTarget.cs
+++ Assets/Scripts/Level/InteractableObjects/StuckTrackTarget.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class StuckTrackTarget : MonoBehaviour
@@ -21,7 +22,7 @@ public class StuckTrackTarget : MonoBehaviour
 
     private void Update()
     {
-        if (StuckedObject.StuckedToCollider?.transform != StuckTo)
+        if (StuckedObject.StuckedToCollider?.transform != StuckTo || StuckedObject.StuckedToCollider.IsDestroyed())
         {
             Destroy(gameObject);
         }
