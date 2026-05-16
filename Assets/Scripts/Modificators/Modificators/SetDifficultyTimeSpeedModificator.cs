5c6a870fad9c811eefa23a367c3dd84a1346cf0d
F33$H <TheDron2006@gmail.com>
2026-05-16 17:50:44 +0300

    added DoubleDifficultyTimeSpeed modificator

---
+++ Assets/Scripts/Modificators/Modificators/SetDifficultyTimeSpeedModificator.cs
@@ -0,0 +1,22 @@
+﻿
+public class SetDifficultyTimeSpeedModificator : AbstractModificator
+{
+    public float DifficultyTimeSpeedMult = 1f;
+
+    public override void OnModificatorAdded()
+    {
+        base.OnModificatorAdded();
+
+        DifficultyManager.Instance.TimeSpeedMultiplier *= DifficultyTimeSpeedMult;
+    }
+
+    public override void OnModificatorRemoved()
+    {
+        base.OnModificatorRemoved();
+
+        if (DifficultyManager.Instance != null)
+        {
+            DifficultyManager.Instance.TimeSpeedMultiplier /= DifficultyTimeSpeedMult;
+        }
+    }
+}
