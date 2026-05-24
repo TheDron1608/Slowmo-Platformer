9bb16fffa49f03f89976a7d3f72e49da888c3ce7
F33$H <TheDron2006@gmail.com>
2026-05-25 02:09:03 +0300

    refactored bat damaging work

---
+++ Assets/Scripts/Level/Effects/SimpleEffects/EffectOnHardStunOnly.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+[AllowEffectWithSenderReceiveNull]
+public class EffectOnHardStunOnly : AbstractEffectWithSender, IMultiplierableEffect
+{
+    public List<AbstractEffect> EffectsOnHasStun = new();
+    public List<AbstractEffect> EffectsOnNoStun = new();
+
+    private float _effectMultiplier = 1f;
+
+    public float EffectMultiplier
+    {
+        get => _effectMultiplier;
+        set => _effectMultiplier = value;
+    }
+
+    protected override void OnReceivedSender(MonoBehaviour sender)
+    {
+        AffectedObject.ApplyEffect(
+            AffectedObject.GetHasEffect<HardStun>() ? EffectsOnHasStun : EffectsOnNoStun,
+            sender,
+            EffectMultiplier
+            );
+
+        RemoveSelf();
+    }
+
+    public override bool Equals(AbstractEffect other)
+    {
+        return
+            base.Equals(other) &&
+            EffectsOnHasStun == (other as EffectOnHardStunOnly).EffectsOnHasStun &&
+            EffectsOnNoStun == (other as EffectOnHardStunOnly).EffectsOnNoStun;
+    }
+}
