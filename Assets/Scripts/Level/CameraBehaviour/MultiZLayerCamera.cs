4bac9cfde9409d9fd9d806e059e9016ac7c01bb5
F33$H <TheDron2006@gmail.com>
2026-05-29 04:34:06 +0300

    Reapply "fixed sword character's track is stucked when changing z layer"
    This reverts commit f30b448380341dca540e56d97c7b2d81b711d782.

--- Assets/Scripts/Level/CameraBehaviour/MultiZLayerCamera.cs
+++ Assets/Scripts/Level/CameraBehaviour/MultiZLayerCamera.cs
@@ -55,7 +55,16 @@ public class MultiZLayerCamera : MonoBehaviour
     {
         if (_cameraTrackComponent.TrackTargets.Count > 0)
         {
-            CurrentZLayer = LayerManager.Instance.GetZLayerOfGameObject(_cameraTrackComponent.TrackTargets[0].gameObject);
+            ZIndexLayer maxZLayer = null;
+            foreach (var trackTarget in _cameraTrackComponent.TrackTargets)
+            {
+                ZIndexLayer currentTrackTargetZLayer = LayerManager.Instance.GetZLayerOfGameObject(_cameraTrackComponent.TrackTargets[0].gameObject);
+                if ((maxZLayer?.ZIndex ?? int.MinValue) < currentTrackTargetZLayer.ZIndex)
+                {
+                    maxZLayer = currentTrackTargetZLayer;
+                }
+            }
+            CurrentZLayer = maxZLayer;
         }
     }
 }
