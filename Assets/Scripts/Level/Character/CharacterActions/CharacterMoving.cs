3c558c46d42a6ef99276495f557921e3300af349
F33$H <TheDron2006@gmail.com>
2026-06-29 13:59:02 +0300

    better sky at gameplay

--- Assets/Scripts/Level/Character/CharacterActions/CharacterMoving.cs
+++ Assets/Scripts/Level/Character/CharacterActions/CharacterMoving.cs
@@ -235,6 +235,6 @@ public class CharacterMoving : AbstractCharacterComponent
 
     public void Animator_PlayStepSound()
     {
-        StepSound.PlaySound();
+        //StepSound.PlaySound();
     }
 }
