c748654d2228c8e84d1db1deaf478236a866f3ec
F33$H <TheDron2006@gmail.com>
2026-08-07 23:07:30 +0300

    added start weapon modificator

---
+++ Assets/Scripts/Modificators/Modificators/GivePlayerHoldableIfUnarmed.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Collections.Generic;
+
+public class GivePlayerHoldableIfUnarmed : AbstractModificator
+{
+    public List<Holdable> PossibleHoldables = new();
+
+    public override void OnLevelGenerated()
+    {
+        foreach (var playerCharacter in TeamManager.Instance.GetTeamDataByTeam(TeamManager.Teams.PLAYER).GetTeamMembers())
+        {
+            if (playerCharacter.CharComponents.CharacterHolding.CurrentHoldObject == null)
+            {
+                playerCharacter.CharComponents.CharacterHolding.GiveNewHoldable(NumberMath.PickRandomItem(PossibleHoldables));
+            }
+        }
+    }
+}
