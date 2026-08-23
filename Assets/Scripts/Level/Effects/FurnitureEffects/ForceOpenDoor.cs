f093a2326259d14c2c83eaa3fb35ce9e7217ae03
F33$H <TheDron2006@gmail.com>
2026-08-23 23:30:31 +0300

    fixed kick animation stuck while sliding on wall

---
+++ Assets/Scripts/Level/Effects/FurnitureEffects/ForceOpenDoor.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+[AllowEffectWithSenderReceiveNull]
+public class ForceOpenDoor : AbstractEffectWithSender
+{
+    protected override void OnReceivedSender(MonoBehaviour sender)
+    {
+        if (AffectedObject.TryGetComponent(out OnInteractToggleOpenDoor door))
+        {
+            door.ForceOpen(sender?.gameObject);
+        }
+        RemoveSelf();
+    }
+
+    public override bool ApplyCondition(ObjectEffectsReceiver affectWho, MonoBehaviour sender)
+    {
+        return base.ApplyCondition(affectWho, sender) && affectWho.TryGetComponent(out OnInteractToggleOpenDoor d);
+    }
+}
